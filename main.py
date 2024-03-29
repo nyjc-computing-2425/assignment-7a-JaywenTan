b5e70073f9be76150ffe4b562868586e3e943d11
JaywenTan <160487275+JaywenTan@users.noreply.github.com>
2024-03-29 14:41:32 +0000

    Final Submission

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 # Write a function to convert numbers to text numerals
 
 def text_numeral(num):
-    pass
+  """
+  converts a number from integer form to string form
+  parameters:
+  num - int
+  """
+  x = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+    }
+  y = list(x.keys())
+  y.sort(reverse=True)
+  str = ""
+  for e in y:
+    if num >= e:
+      num -= e
+      str += x[e]
+      if num != 0:
+        str += " "
+  return str
+
